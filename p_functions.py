d3df81676bf7a628d45e41f8d642390839ba8b51
Jeffrey Wainaina <68434262+geniusdev26@users.noreply.github.com>
2021-01-29 22:41:13 +0300

    Add files via upload

---
+++ p_functions.py
@@ -0,0 +1,13 @@
+"""
+This is an example of a funtion in python.
+Functions are used to make code cleaner by calling them
+in different parts of your code.
+"""
+#A PYTHON FUNCTION THAT ADDS ONE TO ANOTHER NUMBER
+def addOne (Number):
+    Number += 10
+    Output = Number
+    var1 = Output + 1
+    return Output + var1
+    
+print(addOne(30))
