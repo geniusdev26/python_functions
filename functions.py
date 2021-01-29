dcf2423e565630346d76dbc00e3a06035388ede4
Jeffrey Wainaina <68434262+geniusdev26@users.noreply.github.com>
2021-01-29 23:16:25 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,12 @@
+"""
+Function Structure
+def FunctionName (input):
+    Action
+    return Output
+"""
+def addOne (Number):
+    Output = Number +=1
+    return Output
+Var = 0
+VarNew = addOne (Var)
+print (Var)
